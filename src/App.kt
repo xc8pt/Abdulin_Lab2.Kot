2b96ff7302dfd7955643933274d18bcc4373e9fe
Osama Bin Ladden <nzaicenko531@gmail.com>
2025-09-20 12:07:33 +0300

    Create program

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello world!")
+    print("Введите первое число: ")
+    val number1 = readln().toInt()
+    print("Введите второе число: ")
+    val number2 = readln().toInt()
+
+    if (number1 > number2){
+        println("Число $number1 больше чем число $number2")
+    } else {
+        println("Число $number2 больше чем число $number1")
+    }
 }
