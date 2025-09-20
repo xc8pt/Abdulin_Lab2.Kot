99c03d25df5d8edff5d29e2193262dd71378eb5a
Osama Bin Ladden <nzaicenko531@gmail.com>
2025-09-20 12:24:45 +0300

    1

--- src/App.kt
+++ src/App.kt
@@ -1,12 +1,26 @@
-fun main() {
-    print("Введите первое число: ")
-    val number1 = readln().toInt()
-    print("Введите второе число: ")
-    val number2 = readln().toInt()
+//fun main() {
+//    print("Введите первое число: ")
+//    val number1 = readln().toInt()
+//    print("Введите второе число: ")
+//    val number2 = readln().toInt()
+//
+//    if (number1 > number2){
+//        println("Число $number1 больше чем число $number2")
+//    } else {
+//        println("Число $number2 больше чем число $number1")
+//    }
+//}
+fun main(){
+//    print("Введите урон: ")
+//    val n1 = readln().toInt()
+//    print("Щит активен? (введите да или нет): ")
+//    val n2 = readln()
+//    val uron = n1
+//    val shit = n1 / 2
+//    if (n2 == "да"){
+//        println("Полученный урон: $shit")
+//    } else {
+//        println("Полученный урон: $uron")
+//    }
 
-    if (number1 > number2){
-        println("Число $number1 больше чем число $number2")
-    } else {
-        println("Число $number2 больше чем число $number1")
-    }
 }
